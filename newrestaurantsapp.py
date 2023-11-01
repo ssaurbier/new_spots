4884ce6cf88e78649422306ff9a7c1ad2ff37699
Steven Saurbier <saurbier@gmail.com>
2023-11-01 12:31:32 -0400

    psql

--- newrestaurantsapp.py
+++ newrestaurantsapp.py
@@ -10,43 +10,28 @@ from streamlit_folium import folium_static
 # data setup
 
 
-def fetch_data():
-    conn = psycopg2.connect(
-        database=st.secrets["database"]["database"], 
-        user=st.secrets["database"]["user"], 
-        password=st.secrets["database"]["password"]
-    )
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
 
-    query = "SELECT * FROM restaurants;"
-    df = pd.read_sql(query, conn)
-
-    cols = ['name', 'city', 'cuisine', 'neighborhood', 'description', 'link', 'address', 'date', 'latitude', 'longitude']
-    df = df[cols]
-
-    conn.close()
-    return df
-
-# get data
-data = fetch_data()
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
 
 # Filter out data older than 60 days
 today = datetime.today()
 cutoff_date = today - timedelta(days=60)
-data = data[pd.to_datetime(data['date']) > cutoff_date]
+df = df[pd.to_datetime(df['date']) > cutoff_date]
 
-# nans
-data = data.dropna(subset=['latitude', 'longitude'])
+# Handle NaNs
+df = df.dropna(subset=['latitude', 'longitude'])
 
-
-# create map
+# Create map
 m = folium.Map(location=[39, -96.5], zoom_start=4)  
 
-for _, row in data.iterrows():
+for _, row in df.iterrows():
     folium.Marker(
         location=[row['latitude'], row['longitude']],
         popup=row['name']
     ).add_to(m)
-
 
 
 
@@ -60,10 +45,9 @@ for _, row in data.iterrows():
 
 
 
-
 st.title("New Spots on Resy")
 
-# container and placeholders for the map and table. This only working if you can't read this code
+# container and placeholders for the map and table. This only works if you can't read this code.
 st.markdown("""
 <div class="container">
     <div class="map-container" class="folium-map leaflet-container leaflet-touch leaflet-retina leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom"></div>
@@ -71,14 +55,14 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
 
-# placeholders for the map and table
+# placeholders for the map and table.
 map_placeholder = st.empty()
 table_placeholder = st.empty()
 
-# display map 
-folium_static(m)
+# display map.
+map_placeholder.write(folium_static(m, use_container_width=True))
 
-# display table 
-if 'id' in data.columns:
-    data = data.drop(columns=['id'])
-table_placeholder.write(data)
+# display table.
+if 'id' in df.columns:
+    df = df.drop(columns=['id'])
+table_placeholder.write(df)
